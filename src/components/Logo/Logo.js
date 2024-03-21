a7ed711362be4ca008d908788e19269fcd219743
gmc314 <grant.cheng1@gmail.com>
2024-03-21 18:29:30 -0400

    add imagelink component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,14 +2,14 @@ import React from "react";
 import Tilt from 'react-parallax-tilt';
 import robot from './robot.png';
 
-const Navigation = () => {
+const Logo = () => {
   return (
     <Tilt style={{ height: '100px', width: '200px'}} className="Tilt">
       <div>
-        <img alt="logo" src={robot}></img>
+        <img longdesc="https://www.freepik.com/icon/robot_630426#fromView=search&page=1&position=5&uuid=553f2d3b-6ebe-4c59-bdc8-7a2cc9cd82b4" alt="Icon by Freepik" src={robot}></img>
       </div>
     </Tilt>
   );
 }
 
-export default Navigation
+export default Logo
