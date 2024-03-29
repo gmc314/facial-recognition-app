782d2292f70eab7c0d047936c054c08c33a357b7
gmc314 <grant.cheng1@gmail.com>
2024-03-29 13:55:11 -0400

    add alt tage

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ class App extends Component {
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/FaceDetection/FaceDetection.js
+++ src/components/FaceDetection/FaceDetection.js
@@ -3,9 +3,8 @@ import React from "react";
 const FaceDetection = () => {
   return (
     <div className="center"> 
-      <img width="200" src={'https://hips.hearstapps.com/hmg-prod/images/beethoven-600x600.jpg?resize=1200:*'} />
-    </div>
-  
+      <img width="200" alt="Photo for face-detection" src={'https://hips.hearstapps.com/hmg-prod/images/beethoven-600x600.jpg?resize=1200:*'} />
+    </div> 
   );
 }
 
