47888590b110d2c7730ac9afbbead5ba72e40a1e
gmc314 <grant.cheng1@gmail.com>
2024-03-28 21:32:24 -0400

    add onInputChange for input box

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type='cobweb' bg={true} />
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      { /*<FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    }
+  };
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        <ParticlesBg type='cobweb' bg={true} />
+        <Navigation />
+        <Logo />
+        <ImageLinkForm onInputChange={ this.onInputChange }/>
+        { /*<FaceRecognition />*/}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
   return (
     <div>
       <p className="f3">
@@ -9,7 +9,7 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-3">
-          <input className="f4 pa2 w-70 center" placeholder="Enter a URL" type="text"/>
+          <input className="f4 pa2 w-70 center" placeholder="Enter a URL" onChange={ onInputChange } type="text"/>
           <button className="w-30 grow f4 link ph3 pv dib black bg-orange"> Detect Faces </button>  
         </div>
       </div>
