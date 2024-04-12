1ca17ba8a9ef057ffa89e9f6923ed954ddb9ddcc
gmc314 <grant.cheng1@gmail.com>
2024-04-12 13:54:26 -0400

    quick fix of entries

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
           })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, {entries: count.entries}))
+              this.setState(Object.assign(this.state.user, {entries: count}))
             })
             .catch(console.log)
         }
