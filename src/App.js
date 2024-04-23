1198bf317b1bc2f6e010fba15173afc4cd2ea4b5
gmc314 <grant.cheng1@gmail.com>
2024-04-23 15:53:58 -0400

    deploy to render.com

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageURL: this.state.input});
     const { user, input } = this.state;
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://face-recognition-server-1yyw.onrender.com/imageurl", {
         method: "post",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
@@ -92,7 +92,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://face-recognition-server-1yyw.onrender.com/image", {
             method: "put",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends Component {
   }
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/signin", {
+    fetch("https://face-recognition-server-1yyw.onrender.com/signin", {
       method: "post",
       headers: {
         "Content-Type": "application/json"
